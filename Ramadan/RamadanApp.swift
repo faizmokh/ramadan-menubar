8c72a9b1aac8fb337ea0d10e50059d3e718f29b6
Faiz Mokhtar <mfmokhtar@gmail.com>
2025-02-15 10:36:13 +0800

    Initial Commit

---
+++ Ramadan/RamadanApp.swift
@@ -0,0 +1,17 @@
+//
+//  RamadanApp.swift
+//  Ramadan
+//
+//  Created by Faiz Mokhtar on 15/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct RamadanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RamadanTests/RamadanTests.swift
@@ -0,0 +1,17 @@
+//
+//  RamadanTests.swift
+//  RamadanTests
+//
+//  Created by Faiz Mokhtar on 15/02/2025.
+//
+
+import Testing
+@testable import Ramadan
+
+struct RamadanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
