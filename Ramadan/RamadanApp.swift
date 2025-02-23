0f4e2cfc18fb36a13b1f669c355562d057a16dc2
Faiz Mokhtar <mfmokhtar@gmail.com>
2025-02-23 21:59:20 +0800

    Implement check update

--- Ramadan/RamadanApp.swift
+++ Ramadan/RamadanApp.swift
@@ -6,12 +6,21 @@
 //
 
 import SwiftUI
+import Sparkle
 
 @main
 struct RamadanApp: App {
     @StateObject private var viewModel = RamadanViewModel()
     
     @Environment(\.openSettings) private var openSettings
+
+    private let updaterController: SPUStandardUpdaterController
+    
+    init() {
+        // If you want to start the updater manually, pass false to startingUpdater and call .startUpdater() later
+        // This is where you can also pass an updater delegate if you need one
+        updaterController = SPUStandardUpdaterController(startingUpdater: true, updaterDelegate: nil, userDriverDelegate: nil)
+    }
 
     var body: some Scene {
         MenuBarExtra {
@@ -31,6 +40,8 @@ struct RamadanApp: App {
                 openSettings()
             })
                 .keyboardShortcut(",", modifiers: [.command])
+            CheckForUpdatesView(updater: updaterController.updater)
+                .keyboardShortcut("u", modifiers: [.command])
             Button("Quit", action: quit)
                 .keyboardShortcut("q", modifiers: [.command])
         } label: {
